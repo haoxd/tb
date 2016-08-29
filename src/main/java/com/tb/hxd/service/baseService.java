12b2ef34a6d4e9dca3cdaeb859c5f430b69faa79
haoxd <786436534@qq.com>
2016-08-29 16:08:55 +0800

    基于泛型化开发service通用接口

--- src/main/java/com/tb/hxd/service/baseService.java
+++ src/main/java/com/tb/hxd/service/baseService.java
@@ -1,7 +1,23 @@
 package com.tb.hxd.service;
 
-public abstract interface baseService {
+import java.util.List;
 
+import javax.xml.rpc.ServiceException;
+
+import com.tb.hxd.service.expetion.ServiceExpetion;
+
+public abstract interface baseService {
+	
+	public <T> void update(Class<T> clazz,Object t) throws ServiceExpetion;
+	
+	public <T> void insert (Class<T> clazz,Object t) throws ServiceExpetion;
+	
+	public <T> List<T> getData(Class<T> clazz,Object prams) throws ServiceExpetion;
 	
 	
+	public <T> T getDataSingle(Class<T> clasz, Object param) throws ServiceException;
+	
+	public <T> void del(Class<T> t ,Object prams) throws ServiceExpetion;
+	
+
 }
